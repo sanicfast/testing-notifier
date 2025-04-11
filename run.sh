b5559f41077e7a7e8dd57b9426199e2b42b022a3
Tom Norton <sanic.go.fast.step.it.up@gmail.com>
2025-04-11 15:17:45 -0500

    added kristine to run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ export PYTHONUNBUFFERED=1
 source .venv/bin/activate
 python userbrain.py tom > logs/userbrain-t.log 2>&1 & echo $!
 python usertesting.py tom > logs/usertesting-t.log 2>&1 & echo $!
+python userbrain.py kristine > logs/userbrain-k.log 2>&1 & echo $!
+python usertesting.py kristine > logs/usertesting-k.log 2>&1 & echo $!
 pkill chrome
